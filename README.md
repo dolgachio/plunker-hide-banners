10653b313a114eb09bb5712ea75d9f89159fc29e
Stanislav Dolgachov <s.dolgachov@gmail.com>
2019-09-09 18:38:13 +0300

    Hide sidebar by pressing F4

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # plunker-hide-banners
 It is a simple browser plugin for hiding banners in plunker
+It hides banners by default and left sidebar by pressing `F4`
--- plunker-hide-banners.js
+++ plunker-hide-banners.js
@@ -1,1 +1,13 @@
+// Hide banners
 document.documentElement.classList.toggle('plunker-hide-banners');
+
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 115) {
+      // F4
+      toggle();
+    }
+});
+
+function toggle() {
+    document.documentElement.classList.toggle('plunker-hide-sidebar');
+}
