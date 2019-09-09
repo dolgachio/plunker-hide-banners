fe2cfc3569f6e0060f1604b59ad40561ff13a0f6
Stanislav Dolgachov <s.dolgachov@gmail.com>
2019-09-09 12:58:56 +0300

    Hide banners by default without any key pressed

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # plunker-hide-banners
 It is a simple browser plugin for hiding banners in plunker
-
-#### Usage:
-Press `F4` - hides/shows banners on the page
--- plunker-hide-banners.js
+++ plunker-hide-banners.js
@@ -1,10 +1,1 @@
-document.addEventListener('keydown', function(e) {
-    if (e.keyCode == 115) {
-      // F4
-      toggle();
-    }
-  });
-  
-  function toggle() {
-    document.documentElement.classList.toggle('plunker-hide-banners');
-  }
+document.documentElement.classList.toggle('plunker-hide-banners');
